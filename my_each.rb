dd66ccaf9158755e548d3f08c07ffd17dbf1c7e8
Alyssa Brown <alyssa.brown@live.com>
2019-01-23 18:42:52 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,7 @@
 def my_each(array)
-  words=0
-  while words>= array.length
-    yield array[words]
-     
-    words +=1 
+  i=0
+  while i < array.length
+    yield (array[i])
+     i +=1 
   end  
-    
 end
-
-
